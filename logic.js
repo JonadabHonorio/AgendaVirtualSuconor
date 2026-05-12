545cfad3f99a2d786ba6d8a50eecc2c36347eaad
JonadabHonorio <96204594+JonadabHonorio@users.noreply.github.com>
2026-05-12 09:15:11 -0300

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,7 @@
+const searchInput = document.getElementById("search");
+const ramalList = document.getElementById("ramalList");
+const emptyMessage = document.getElementById("emptyMessage");
+
+const ramalList = [
+  { setor: "Tecnologia da Informação", nome: "Setorial", ramal: "202" }
+];
