6f5d32d0b77a0d9f6006a925025637618ee6b5c8
JonadabHonorio <96204594+JonadabHonorio@users.noreply.github.com>
2026-08-06 11:50:18 -0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -3,6 +3,9 @@ const inputSearch = document.getElementById("inputSearch");
 const ramalListDefault = document.querySelectorAll(".ramalListDefault");
 const ramalListFirst = document.querySelector(".ramalListFirst");
 const ramalListLast = document.querySelector(".ramalListLast");
+const searchCounterReturn = document.getElementById("searchCounterReturn");
+
+
 
 
 const ramalAll = [ ...ramalListDefault , ramalListFirst , ramalListLast]; /*Esse array armazena todas as estruturas do HTML referente aos ramais*/
@@ -13,19 +16,24 @@ function normalizeInput(input) { /* Função responsável por retirar os caracte
 };
 
 
+var teste = contact;
 inputSearch.addEventListener("input", () => { 
     const search = normalizeInput(inputSearch.value); 
+    
          ramalAll.forEach(contact => { 
             const htmlForText = normalizeInput(contact.textContent);
             if (htmlForText.includes(search)) { 
-                contact.style.display = "";  /* O campo está em branco para não influênciar no layaut dos elementos que contêm Flex-Box*/
-                contact.classList.add("ramalListReturned");
+                    contact.style.display = "";  /* O campo está em branco para não influênciar no layaut dos elementos que contêm Flex-Box*/
+                    contact.classList.add("ramalListReturned");
+                    
             }else { 
-                contact.style.display = "none"; 
+                    contact.style.display = "none"; 
             } 
         } 
      );  
   });
+
+console.log(teste);
 
 inputSearch.addEventListener("input", () => { /*Função responsável por remover o layaut especial das agendas pesquisadas para a padrão após pesquisa */
     const search = normalizeInput(inputSearch.value);
@@ -38,5 +46,10 @@ inputSearch.addEventListener("input", () => { /*Função responsável por remove
         return;
     }
 })
+
+
+
+
+ searchCounterReturn.textContent = "Testinho" 
 
 
