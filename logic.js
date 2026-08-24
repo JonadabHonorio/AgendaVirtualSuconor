8d0079e35ec1c80fcb321ca286a809a0093d5ba1
JonadabHonorio <96204594+JonadabHonorio@users.noreply.github.com>
2026-08-24 17:00:17 -0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -3,15 +3,16 @@ const ramalListDefault = document.querySelectorAll(".ramalListDefault");
 const ramalListFirst = document.querySelector(".ramalListFirst");
 const ramalListLast = document.querySelector(".ramalListLast");
 const searchCounterReturn = document.getElementById("searchCounterReturn");
+const container_ramalList = document.getElementById("container_ramalList");
 const container_ramalNotFaund = document.getElementById("container_ramalNotFaund");
 const sectorNameResponsive = document.querySelectorAll(".sectorNameResponsive");
 const sectorName500px = document.querySelectorAll(".sectorName500px");
 const container_ramalNumber = document.querySelectorAll(".container_ramalNumber");
 const container_Father = document.getElementById("container_Father");
-const windowsBrowserDimension = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth; /*Captura a largura da janela do usuário*/
+const windowsBrowserDimension = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth; /* Captura a largura da janela do usuário */
 
 
-const ramalAll = [ ...ramalListDefault , ramalListFirst , ramalListLast]; /*Esse array armazena todas as estruturas do HTML referente aos ramais*/
+const ramalAll = [ ...ramalListDefault , ramalListFirst , ramalListLast]; /* Esse array armazena todas as estruturas do HTML referente aos ramais */
 
 
 function normalizeInput(input) { /* Função responsável por retirar os caracteres especiais inputados na barra de pesquisa */
@@ -19,13 +20,13 @@ function normalizeInput(input) { /* Função responsável por retirar os caracte
 };
 
 
-inputSearch.addEventListener("input", () => {  /*Função responsável por tratar o input da barra de pesquisa e comparar com a lista de ramais existente, em seguida retorna quando encontrado */
+inputSearch.addEventListener("input", () => {  /* Função responsável por tratar o input da barra de pesquisa e comparar com a lista de ramais existente, em seguida retorna quando encontrado */
     const search = normalizeInput(inputSearch.value); 
     let searchCounterIncrement = 0;  
          ramalAll.forEach(contact => { 
             const htmlForText = normalizeInput(contact.textContent);
             if (htmlForText.includes(search)) { 
-                    contact.style.display = "";  /* O campo está em branco para não influênciar no layaut dos elementos que contêm Flex-Box*/
+                    contact.style.display = "";  /* O campo está em branco para não influênciar no layaut dos elementos que contêm Flex-Box */
                     contact.classList.add("ramalListReturned");
                     searchCounterIncrement++;                               
             }else { 
@@ -33,7 +34,7 @@ inputSearch.addEventListener("input", () => {  /*Função responsável por trata
             } 
         } 
      ); 
-     searchCounterReturn.innerHTML = `${searchCounterIncrement} Ramais Encontrados`; /*Parte responsável por retornar a quantidade de ramais*/
+     searchCounterReturn.innerHTML = `${searchCounterIncrement} Ramais Encontrados`; /* Parte responsável por retornar a quantidade de ramais */
             if(searchCounterIncrement == 1 ) {
                      searchCounterReturn.innerHTML =`${searchCounterIncrement} Ramal Encontrado`;
 
@@ -43,11 +44,10 @@ inputSearch.addEventListener("input", () => {  /*Função responsável por trata
             }else {
                 container_ramalNotFaund.style.display = "none";
             }
-
 });
 
 
-inputSearch.addEventListener("input", () => { /*Função responsável por remover o layaut especial das agendas pesquisadas para a padrão após pesquisa */
+inputSearch.addEventListener("input", () => { /* Função responsável por remover o layaut especial das agendas pesquisadas para a padrão após pesquisa */
     const search = normalizeInput(inputSearch.value);
 
     if (search === "") {
@@ -63,14 +63,23 @@ inputSearch.addEventListener("input", () => { /*Função responsável por remove
 
 document.addEventListener("DOMContentLoaded", () => { /* Função responsável pela responsividade de determinados ramais para Mobile */
     if(windowsBrowserDimension <= 500) {
-        sectorNameResponsive[0].style.display = "none" ;
-        sectorName500px[0].style.display = "block" ;
+        sectorNameResponsive[0].style.display = "none";
+        sectorName500px[0].style.display = "block";
         container_ramalNumber[1].style.bottom = "67px";
-        sectorNameResponsive[1].style.display = "none" ;
-        sectorName500px[1].style.display = "block" ;
+        sectorNameResponsive[1].style.display = "none";
+        sectorName500px[1].style.display = "block";
         container_ramalNumber[10].style.bottom = "67px";
-        sectorNameResponsive[2].style.display = "none" ;
-        sectorName500px[2].style.display = "block" ;
+        sectorNameResponsive[2].style.display = "none";
+        sectorName500px[2].style.display = "block";
         container_ramalNumber[24].style.bottom = "67px";       
   } 
 });
+
+
+inputSearch.addEventListener("keydown", (event) =>{  /*Função responsável por centralizar o ramal procurado após receber o "enter" do teclado */
+        if (event.key === "Enter") {
+            const container_ramalList = document.getElementById("container_ramalList");
+            container_ramalList.scrollIntoView();
+    }
+});
+
